1032522e4efeb8c8fe1b8d200a67ef16cae6f66c
SlobodaJulia <slobodajulia29@gmail.com>
2024-01-28 15:54:18 +0200

    listener

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -1,8 +1,11 @@
 package manager;
 
+import com.google.common.io.Files;
 import org.openqa.selenium.*;
-import org.openqa.selenium.WebDriver;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 public class HelperBase {
     WebDriver wd;
@@ -59,5 +62,37 @@ public class HelperBase {
         }
     }
 
+
+    public boolean isYallaButtonNotActive() {
+        boolean res = isElementPresent(By.cssSelector("button[disabled]"));
+
+        WebElement element = wd.findElement(By.cssSelector("button[type='submit']"));
+        boolean result = element.isEnabled();
+        return res && !result;
+    }
+
+
+    public void getScreen(String link) {
+        TakesScreenshot takesScreenshot=(TakesScreenshot) wd;
+        File tmp =takesScreenshot.getScreenshotAs(OutputType.FILE);
+        try {
+            Files.copy(tmp,new File(link));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clearTextBox(By locator){
+        WebElement el = wd.findElement(locator);
+        String os = System.getProperty("os.name");
+        System.out.println(os);
+        if(os.startsWith("Win")) {
+            el.sendKeys(Keys.CONTROL, "a");
+        }else {
+            el.sendKeys(Keys.COMMAND, "a");
+        }
+        el.sendKeys(Keys.DELETE);
+
+    }
 
 }
--- src/main/java/manager/HelperCar.java
+++ src/main/java/manager/HelperCar.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class HelperCar extends HelperBase{
     public HelperCar(WebDriver wd) {
         super(wd);
@@ -57,5 +60,122 @@ public class HelperCar extends HelperBase{
     public void attachPhoto(String link) {
         WebElement element = wd.findElement(By.id("photos"));
         element.sendKeys(link);
+    }
+    public void searchCurrentMonth(String city, String dateFrom, String dateTo) {
+        clearTextBox(By.id("city"));
+        typeCity(city);
+        clearTextBox(By.id("dates"));
+        click(By.id("dates"));
+        //"1/25/2024", "1/28/2024"   25  28
+        String[] from = dateFrom.split("/"); ///["1"] ["25"] ["2024"]
+
+        String locatorFrom = "//div[text() = ' " + from[1] + " ']";
+        click(By.xpath(locatorFrom));
+        String[] to = dateTo.split("/");
+        click(By.xpath("//div[text() = ' " + to[1] + " ']"));
+
+
+    }
+
+    private void typeCity(String city) {
+        type(By.id("city"), city);
+        click(By.cssSelector("div.pac-item"));
+    }
+
+    public boolean isListOfCarsAppeared() {
+        return isElementPresent(By.cssSelector("a.car-container"));
+    }
+
+    public void searchCurrentYear(String city, String dateFrom, String dateTo) {
+        clearTextBox(By.id("city"));
+        typeCity(city);
+        clearTextBox(By.id("dates"));
+        click(By.id("dates"));
+
+        //"1/20/2024", "5/23/2024"
+        LocalDate now = LocalDate.now();
+        System.out.println(now); //2024-01-14
+        int year = now.getYear();
+        int month = now.getMonthValue();
+        int day = now.getDayOfMonth();
+        LocalDate from =LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern("M/d/yyyy"));
+        LocalDate to =LocalDate.parse(dateTo, DateTimeFormatter.ofPattern("M/d/yyyy"));
+//LocalDate from1 = LocalDate.parse("2013:23/05", DateTimeFormatter.ofPattern("yyyy:dd/MM"));
+        //System.out.println(from1);
+
+        int diffMonth = from.getMonthValue()-month;
+
+        if (diffMonth>0){
+            clickNextMonth(diffMonth);
+        }
+
+        click(By.xpath("//div[text() = ' " + from.getDayOfMonth() + " ']"));
+
+        diffMonth = to.getMonthValue()-from.getMonthValue();
+
+        if (diffMonth>0){
+            clickNextMonth(diffMonth);
+        }
+
+
+//"//div[text() = ' " + from[1] + " ']";
+        String locator = String.format("//div[text() = ' %s ']", to.getDayOfMonth());
+        click(By.xpath(locator));
+    }
+
+    private void clickNextMonth(int diffMonth) {
+        for (int i = 0; i < diffMonth; i++) {
+            click(By.cssSelector("button[aria-label ='Next month']"));
+
+        }
+    }
+
+    public void searchAnyPeriod(String city, String dateFrom, String dateTo) {
+        clearTextBox(By.id("city"));
+        typeCity(city);
+        clearTextBox(By.id("dates"));
+        click(By.id("dates"));
+        LocalDate now = LocalDate.now();
+        LocalDate from = LocalDate.parse(dateFrom,DateTimeFormatter.ofPattern("M/d/yyyy"));
+        LocalDate to = LocalDate.parse(dateTo,DateTimeFormatter.ofPattern("M/d/yyyy"));
+        int diffYear;
+        int diffMonth;
+        ///***from
+        diffYear = from.getYear()-now.getYear();
+        if (diffYear==0){
+            diffMonth = from.getMonthValue()-now.getMonthValue();
+        }else {
+            diffMonth = 12-now.getMonthValue()+from.getMonthValue();
+        }
+        clickNextMonth(diffMonth);
+        String locator = String.format("//div[text() = ' %s ']", from.getDayOfMonth());
+        click(By.xpath(locator));
+
+        ///***to
+        diffYear = to.getYear()-from.getYear();
+        if(diffYear==0){
+            diffMonth = to.getMonthValue()-from.getMonthValue();
+        }else {
+            diffMonth = 12 - from.getMonthValue() + to.getMonthValue();
+        }
+        clickNextMonth(diffMonth);
+        locator = String.format("//div[text() = ' %s ']", to.getDayOfMonth());
+        click(By.xpath(locator));
+    }
+
+    public void navigateByLogo() {
+        click(By.cssSelector("a.logo"));
+    }
+
+    public void searchNotValidPeriod(String city, String dateFrom, String dateTo) {
+        typeCity(city);
+        clearTextBox(By.id("dates"));
+        type(By.id("dates"), dateFrom+" - "+dateTo);
+        click(By.cssSelector("div.cdk-overlay-backdrop"));
+    }
+
+    public boolean isErrorDisplayed(String message) {
+        String text = wd.findElement(By.cssSelector("div.ng-star-inserted")).getText();
+        return text.equals(message);
     }
 }
